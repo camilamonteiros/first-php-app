41e85c31f8d06626f6b1e15abf5860763c2359ad
camilamonteiros <camilamonteirodesign@gmail.com>
2022-09-03 13:02:45 -0300

    bugs done

--- functions/url.php
+++ functions/url.php
@@ -1,7 +1,7 @@
 <?php
 function url_redirect($values = []){
     $buildQueryString = ($values);
-    header('Location: http://localhost/first-php-app?'. $buildQueryString);
+    header('Location: http://localhost/first-php-app/?'. $buildQueryString);
     exit;
 }
 ?>
